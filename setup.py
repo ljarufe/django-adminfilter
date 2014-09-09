401b30f8d7dfcc17196a978e0f2171a30e598b76
Luis Jarufe <luisjarufe@gmail.com>
2014-09-09 13:03:13 -0500

    setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+    name='django-adminfilters',
+    version='1.0',
+    description='Django Admin Filters',
+    author='django',
+    url='https://github.com/ljarufe/django-adminfilter',
+    packages = ['adminfilter',]
+)
