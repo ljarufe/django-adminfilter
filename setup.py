3cf02c3a5b50eceed2f69a69113b0ddab19f6a68
Luis Jarufe <luisjarufe@gmail.com>
2014-09-10 15:02:30 -0500

    include all packages

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
+
 from distutils.core import setup
+from setuptools import setup, find_packages
+
 setup(
     name='django-adminfilters',
     version='1.0',
     description='Django Admin Filters',
     author='django',
     url='https://github.com/ljarufe/django-adminfilter',
-    packages = ['adminfilter',]
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
 )
